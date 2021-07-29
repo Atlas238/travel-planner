48bd0f32b142a5b57bffa42cda44c02e119e42e0
Zach Maurer <zmaurer88@yahoo.com>
2021-07-29 11:08:50 -0700

    location routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // SERVER FILE
 const express = require('express');
 const sequelize = require('./config/connection');
+require('dotenv').config();
+const userRoutes = require('./userRoutes');
+router.use('/users', userRoutes);
 
 const app = express();
 const PORT = process.env.PORT|| 3000;
@@ -12,3 +15,42 @@ app.use(express.static('public'));
 sequelize.sync().then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
+router.get('/api/locations', async (req, res) => {
+    const locationData = await User.findAll().catch((err) => {
+      res.json(err);
+    });
+    res.json(locationData);
+  });
+
+  router.post('/api/locations', async (req, res) => {
+    try {
+      const locationData = await Location.create(req.body);
+      res.status(200).json(locationData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.delete('/api/locations/:id', async (req, res) => {
+    const locationData = await Location.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).catch((err) => res.json(err));
+    res.json(locationData);
+  });
+
+  router.get('/api/locations/:id', async (req, res) => {
+    const locationData = await Location.findByPk(req.params.id).catch((err) =>
+      res.json(err)
+    );
+    res.json(locationData);
+  });
+
+
+
+
+
+
+module.exports = router;
