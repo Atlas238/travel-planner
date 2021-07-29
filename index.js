822172eb41ed6a6c451aa2875c9e5cb49c7ff13f
Atlas <jack@bentsen.us>
2021-07-29 11:19:55 -0700

    reformatting

--- index.js
+++ index.js
@@ -16,41 +16,4 @@ sequelize.sync().then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
 
-router.get('/api/locations', async (req, res) => {
-    const locationData = await User.findAll().catch((err) => {
-      res.json(err);
-    });
-    res.json(locationData);
-  });
-
-  router.post('/api/locations', async (req, res) => {
-    try {
-      const locationData = await Location.create(req.body);
-      res.status(200).json(locationData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-
-  router.delete('/api/locations/:id', async (req, res) => {
-    const locationData = await Location.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).catch((err) => res.json(err));
-    res.json(locationData);
-  });
-
-  router.get('/api/locations/:id', async (req, res) => {
-    const locationData = await Location.findByPk(req.params.id).catch((err) =>
-      res.json(err)
-    );
-    res.json(locationData);
-  });
-
-
-
-
-
-
 module.exports = router;
