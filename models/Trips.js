62f5b4ba448996544d0185656aa5070b0ed8c35a
Derek DeLizo <delizoderek@gmail.com>
2021-07-29 11:17:45 -0700

    Created the trips model and added table relations to index.js

--- models/Trips.js
+++ models/Trips.js
@@ -3,3 +3,46 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Trips extends Model {}
+
+Trips.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        trip_budget: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        traveller_amount:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+        },
+        traveller_id: {
+            type: DataTypes.INTEGER,
+            unique: false,
+            references:{
+                model:'traveller',
+                key:'id',
+            },
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            unique:false,
+            references:{
+                model:'location',
+                key:'id',
+            },
+        },
+    },{
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Trip',
+    }
+);
+
+module.exports = Trips;
